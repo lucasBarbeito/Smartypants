1bcb36f1a9b168d01e2ced29336eacd5491a5cc7
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-10-25 20:25:38 -0300

    tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      'primary': {
+        600: '#2E3440',
+        500: '#3B4252',
+        400: '#434C5E',
+      },
+      'greyscale': {
+        600: '#D8DEE9',
+        500: '#E5E9F0',
+        400: '#ECEFF4',
+      },
+      'secondary': {
+        600: '#5E81AC',
+        500: '#81A1C1',
+        400: '#88C0D0',
+      },
+      'success': '#A3BE8C',
+      'error': '#BF616A',
+    },
   },
   plugins: [],
 }
