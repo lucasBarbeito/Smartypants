089664e489b76ba10c242a9c7c89bbf74c73352e
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-10-31 16:34:33 -0300

    Course

---
+++ src/components/Course/Course.stories.tsx
@@ -0,0 +1,90 @@
+import {Meta, StoryObj} from "@storybook/react";
+import Course from "./index";
+
+const meta: Meta<typeof Course> ={
+    title:'Course',
+    component:Course,
+    tags: ['autodocs'],
+    argTypes:{
+        variant:{
+            options:['in-progress', 'description', 'actionable'],
+            control:{type:'select'},
+            defaultValue:'collapsed'
+        }
+    }
+}
+
+export default meta
+
+type Story = StoryObj<typeof Course>
+
+export const InProgress: Story = {
+    render: (args) => (<Course {...args}/>)
+}
+
+InProgress.args = {
+    variant: 'in-progress',
+    courseName: "How to React",
+    averageRating: 4.5,
+    totalRatings: "2K",
+};
+
+export const Description: Story = {
+    render: (args) => (<Course {...args}/>)
+}
+
+Description.args = {
+    variant: 'description',
+    courseName: "How to React",
+    averageRating: 4.5,
+    totalRatings: "2K",
+    description: "Me not know me small brain"
+};
+
+export const Actionable: Story = {
+    render: (args) => (<Course {...args}/>)
+}
+
+Actionable.args = {
+    variant: 'actionable',
+    courseName: "How to React",
+    averageRating: 4.5,
+    totalRatings: "2K",
+    description: "Me not know me small brain"
+};
+
+export const ActionableChild: Story = {
+    render: (args) => (<Course {...args}/>)
+}
+
+ActionableChild.args = {
+    variant: 'actionable-child',
+    courseName: "How to React",
+    averageRating: 4.5,
+    totalRatings: "2K",
+    description: "Me not know me small brain"
+};
+
+export const NotStartedCourse: Story = {
+    render: (args) => (<Course {...args}/>)
+}
+
+NotStartedCourse.args = {
+    variant: 'course',
+    courseName: "How to React",
+    averageRating: 4.5,
+    totalRatings: "2K",
+    description: "Me not know me small brain"
+};
+
+export const NotStartedCourseDescriptioon: Story = {
+    render: (args) => (<Course {...args}/>)
+}
+
+NotStartedCourseDescriptioon.args = {
+    variant: 'course-description',
+    courseName: "How to React",
+    averageRating: 4.5,
+    totalRatings: "2K",
+    description: "Me not know me small brain"
+};
---
+++ src/components/Course/index.tsx
@@ -0,0 +1,66 @@
+import {cva, VariantProps} from "class-variance-authority";
+import ProgressBar from "../ProgressBar";
+import star from "../Icons/Star.svg"
+import starAction from "../Icons/StarAction.svg"
+import exit from "../Icons/Exit.svg"
+import media from "../../Media.svg"
+
+const courseVariant = cva([
+    'text-greyscale-500',
+],{
+    variants:{
+        variant:{
+            'in-progress':[],
+            'description':[],
+            'actionable':[],
+            'actionable-child':[],
+            'course':[],
+            'course-description':[],
+        }
+    },
+    defaultVariants:{
+        variant:'in-progress'
+    }
+})
+
+export interface CourseProps extends VariantProps<typeof courseVariant>{
+    courseName: string,
+    averageRating: number,
+    totalRatings: string,
+    description: string,
+}
+
+const Course = ({variant, courseName, averageRating, totalRatings, description}:CourseProps) => {
+    const borderRadius : string = variant === 'course' || variant === 'course-description' ? ' rounded-t-lg' : ''
+    return (
+        <div className={courseVariant({variant})} >
+            {( variant === 'in-progress' || variant === 'description' || variant === 'actionable' || variant === 'actionable-child') &&
+                (<ProgressBar variant={'top-rounded'} color={'bg-success'} completed={0}/>)
+            }
+            {( variant === 'in-progress' || variant === 'description' || variant === 'actionable' || variant === 'course' || variant === 'course-description') &&
+                (<img className={'flex self-stretch' + borderRadius} src={media} alt={"Media"}/>)
+            }
+            <div className={'gap-1 px-2 pb-2 bg-primary-400 rounded-b-lg'}>
+                <p className={"text-body-large"}>{courseName}</p>
+                {(variant === 'description' || variant === 'actionable' || variant === 'actionable-child' || variant === 'course-description') &&
+                    (<p className={"text-body-small"}>{description}</p>)
+                }
+                <div className={'flex'}>
+                    <div className={'flex items-center'}>
+                        <img src={star} alt="Star icon" />
+                        <p className={"text-body-small"}>{averageRating}({totalRatings})</p>
+                    </div>
+                    <div className={'grow'}></div>
+                    {(variant === 'actionable' || variant === 'actionable-child')  &&
+                        (<div className={'flex items-center'}>
+                            <img src={starAction} alt="Rate" />
+                            <img src={exit} alt="Star icon" />
+                        </div>)
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Course
