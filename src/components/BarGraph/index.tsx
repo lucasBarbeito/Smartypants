7a1c19f2b15894ca6bbd28510df40ba7f40b30e9
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-11-01 19:31:25 -0300

    Bar graph

--- src/components/BarGraph/index.tsx
+++ src/components/BarGraph/index.tsx
@@ -3,27 +3,31 @@ export interface BarGraphProps {
 }
 
 export interface BarProps {
-
+  percentage : number
 }
 
-const Bar = () => {
-  return (
-    <div className={'flex flex-1 bg-success w-1/12 rounded-lg'}>
-
-    </div>
-  )
-}
 const BarGraph = ({text}:BarGraphProps) => {
+  const percentage = 10
     return (
         <div className="flex bg-primary-400 h-64 w-64">
-          <div className="flex flex-row h-64 w-64">
-            <div className="w-1/4 h-full bg-success flex-1"></div>
-            <div className="w-1/4 h-full bg-success flex-1"></div>
-            <div className="w-1/4 h-full bg-success flex-1"></div>
-            <div className="w-1/4 h-full bg-success flex-1"></div>
+          <div className="flex flex-row px-12 gap-2 py-6 h-64 w-64">
+            <Bar percentage={20}/>
+            <Bar percentage={40}/>
+            <Bar percentage={20}/>
+            <Bar percentage={20}/>
           </div>
         </div>
     )
+}
+
+const Bar = ({percentage}:BarProps) => {
+  return (
+    <div className="flex flex-col flex-1 ">
+      <div className={'flex-1'}></div>
+      <div className={`bg-success h-[${percentage}%] rounded-lg`}></div>
+      <p className={'text-center'}>A</p>
+    </div>
+  )
 }
 
 export default BarGraph
