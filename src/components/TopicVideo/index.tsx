e7cd18bae84ead2672b629fb4e62ea54104e60bd
lucasBarbeito <lucas.barbeito@greencodesoftware.com>
2023-11-01 20:22:25 -0300

    Makes video full screen responsive

--- src/components/TopicVideo/index.tsx
+++ src/components/TopicVideo/index.tsx
@@ -10,7 +10,7 @@ export interface TopicVideoProps extends VariantProps<typeof topicVideoVariant> 
 const TopicVideo = () => {
     return (
         <div className="w-full h-48 md:h-64 lg:h-80 flex-shrink-0 rounded-lg no-scrollbar shadow-lg">
-            <video className="w-full h-full object-cover rounded-lg" controls>
+            <video className="w-full h-full object-cover rounded-lg" controls style={{ objectFit: 'contain' }}>
                 <source src="https://media.geeksforgeeks.org/wp-content/uploads/20190616234019/Canvas.move_.mp4"  type="video/mp4"/>
                 Your browser does not support the video tag.
             </video>
