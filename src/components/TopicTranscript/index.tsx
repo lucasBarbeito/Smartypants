642dfa3e53dbe29b214f0989610176b0145a3e53
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-11-01 21:29:04 -0300

    TopicTranscript

--- src/components/TopicTranscript/index.tsx
+++ src/components/TopicTranscript/index.tsx
@@ -6,7 +6,7 @@ export interface TopicTranscriptProps  {
 const TopicTranscript = ({text}: TopicTranscriptProps) => {
     return (
         <div className="flex flex-col h-full overflow-hidden px-2 py-2 bg-primary-400 rounded-lg">
-            <textarea readOnly className="w-full h-full border-none text-greyscale-500 bg-primary-400 resize-none no-scrollbar font-montserrat">{text}</textarea>
+            <textarea readOnly className="w-full h-full focus:outline-none text-greyscale-500 bg-primary-400 resize-none no-scrollbar font-montserrat">{text}</textarea>
         </div>
     )
 }
